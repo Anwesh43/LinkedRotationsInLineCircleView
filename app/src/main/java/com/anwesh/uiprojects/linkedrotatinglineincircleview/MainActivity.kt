9755d1a85df1646f3d243da038512081416363bf
Anwesh43 <anweshthecool0@gmail.com>
2018-12-22 13:30:44 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrotatinglineincircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrotatinglineincircleview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedrotatinglineincircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.rotatinglineincircleview.RotatingLineInCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RotatingLineInCircleView.create(this)
     }
 }
