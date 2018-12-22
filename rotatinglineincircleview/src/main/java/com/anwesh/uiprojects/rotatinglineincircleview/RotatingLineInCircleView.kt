08fc79aefd0f8da0a98ccaf609ab38d6c6eb03bb
Anwesh43 <anweshthecool0@gmail.com>
2018-12-22 12:53:57 +0530

    addinng RotatingLineInCircleView class

--- rotatinglineincircleview/src/main/java/com/anwesh/uiprojects/rotatinglineincircleview/RotatingLineInCircleView.kt
+++ rotatinglineincircleview/src/main/java/com/anwesh/uiprojects/rotatinglineincircleview/RotatingLineInCircleView.kt
@@ -59,3 +59,21 @@ fun Canvas.drawRLICNode(i : Int, scale : Float, paint : Paint) {
     drawLine(0f, 0f, 0f, -size * sc1, paint)
     restore()
 }
+
+class RotatingLineInCircleView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
