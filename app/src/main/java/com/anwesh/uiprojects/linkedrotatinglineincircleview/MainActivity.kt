bd3af463f02e961f096f380d9dadf11aa15490e8
Anwesh43 <anweshthecool0@gmail.com>
2018-12-22 17:13:23 +0530

    adding OnAnimationListener in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrotatinglineincircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrotatinglineincircleview/MainActivity.kt
@@ -3,14 +3,21 @@ package com.anwesh.uiprojects.linkedrotatinglineincircleview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.rotatinglineincircleview.RotatingLineInCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        RotatingLineInCircleView.create(this)
+        val view : RotatingLineInCircleView = RotatingLineInCircleView.create(this)
         fullScreen()
+        view.addOnAnimationCompleteListener({createToast("animation number $it is complete")},
+                {createToast("animation numbner $it is reset")})
+    }
+
+    private fun createToast(txt : String) {
+        Toast.makeText(this, txt, Toast.LENGTH_SHORT).show()
     }
 }
 
