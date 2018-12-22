3d25d3dd34cf5998ec0aed4daee5cb77c7b5d275
Anwesh43 <anweshthecool0@gmail.com>
2018-12-22 13:11:03 +0530

    adding RLICNode class

--- rotatinglineincircleview/src/main/java/com/anwesh/uiprojects/rotatinglineincircleview/RotatingLineInCircleView.kt
+++ rotatinglineincircleview/src/main/java/com/anwesh/uiprojects/rotatinglineincircleview/RotatingLineInCircleView.kt
@@ -124,4 +124,49 @@ class RotatingLineInCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RLICNode(var i : Int, val state : State = State()) {
+
+        private var next : RLICNode? = null
+
+        private var prev : RLICNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = RLICNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRLICNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RLICNode {
+            var curr : RLICNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
