3b67816a2aee6ce1c0437af7baa42682187fd44b
Anwesh43 <anweshthecool0@gmail.com>
2018-12-22 12:51:46 +0530

    adding drawRLICNode canvas extension method

--- rotatinglineincircleview/src/main/java/com/anwesh/uiprojects/rotatinglineincircleview/RotatingLineInCircleView.kt
+++ rotatinglineincircleview/src/main/java/com/anwesh/uiprojects/rotatinglineincircleview/RotatingLineInCircleView.kt
@@ -32,3 +32,30 @@ fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 
 fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Int.percentileMirror() : Int = 1 - 2 * (this % 2)
+
+fun Canvas.drawRLICNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val r : Float = size/3
+    paint.color = color
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    save()
+    translate(gap * (i +1), h/2)
+    drawCircle(0f, 0f, r, paint)
+    var currDeg : Float = 0f
+    for(j in 0..(rotations - 1)) {
+        val sc : Float = sc2.divideScale(j, rotations)
+        currDeg += MAX_DEG * sc * (1 - 2 * j.percentileMirror())
+    }
+    rotate(currDeg)
+    drawLine(0f, 0f, 0f, -size * sc1, paint)
+    restore()
+}
